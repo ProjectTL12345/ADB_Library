b70e8d2e0fa9b36b114af879fb8fadc2e437a9e6
ProjectTL12345 <kimpiao020@gmail.com>
2021-03-20 11:04:03 +0900

    Update: Fix bugs

--- tasks/OpenApp.go
+++ tasks/OpenApp.go
@@ -1,1 +1,14 @@
 package tasks
+
+import (
+	"github.com/ProjectTL12345/ADB_Library/utils"
+	"log"
+	"os/exec"
+)
+
+func OpenApp(packageName string) {
+	log.Println("[ADB_Library] Open app with package name for:", packageName)
+
+	cmd := exec.Command("adb", "shell", "am", "start", "-n", packageName)
+	utils.Action(cmd, 200)
+}
